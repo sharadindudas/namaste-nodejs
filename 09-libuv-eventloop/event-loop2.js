4ea98d79ebbd791b08cbded23ded20fbeb133d60
sharadindu das <sharadindudas774@gmail.com>
2024-12-06 20:00:00 +0530

    feat: Added more event loop code

---
+++ 09-libuv-eventloop/event-loop2.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+const a = 100;
+setImmediate(() => console.log("setImmediate"));
+Promise.resolve().then(() => console.log("Promise"));
+fs.readFile("./file.txt", "utf-8", () => {
+  console.log("File reading cb");
+});
+setTimeout(() => console.log("Timer expired"), 0);
+process.nextTick(() => console.log("process.nextTick"));
+function print() {
+  console.log("A:", a);
+}
+print();
+console.log("Last line of the file");
+
+/*
+  Output
+  ------
+  A: 100
+  Last line of the file
+  process.nextTick
+  Promise
+  Timer expired
+  setImmediate
+  file reading cb
+*/
