9d8fca09f59e6bf087305e6d8fccafe0cb729ed8
sharadindu das <sharadindudas774@gmail.com>
2025-01-09 18:52:02 +0530

    feat: Routes and route handlers

--- season-02/devtinder-backend/app.js
+++ season-02/devtinder-backend/app.js
@@ -3,17 +3,31 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.use("/", (req, res) => {
-  res.send("Namaste Sharadindu!");
-});
+// app.use(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("Route handler 1");
+//     res.send("Route handler 1");
+//     next();
+//   },
+//   (req, res) => {
+//     console.log("Route handler 2");
+//     res.send("Route handler 2");
+//   }
+// );
 
-app.use("/test", (req, res) => {
-  res.send("Testing");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("Hello");
-});
+// app.use(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("Route handler 1");
+//     next();
+//     res.send("Route handler 1");
+//   },
+//   (req, res) => {
+//     console.log("Route handler 2");
+//     res.send("Route handler 2");
+//   }
+// );
 
 app.listen(PORT, () => {
   console.log(`Server started on PORT ${PORT}`);
