f54e20f236cd713027c118b733c06f704fda1eea
sharadindu das <sharadindudas774@gmail.com>
2025-01-09 19:44:16 +0530

    feat: Learned about middlewares

--- season-02/devtinder-backend/app.js
+++ season-02/devtinder-backend/app.js
@@ -3,31 +3,25 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-// app.use(
-//   "/user",
-//   (req, res, next) => {
-//     console.log("Route handler 1");
-//     res.send("Route handler 1");
-//     next();
-//   },
-//   (req, res) => {
-//     console.log("Route handler 2");
-//     res.send("Route handler 2");
-//   }
-// );
+// Middlewares
+app.use("/admin", (req, res, next) => {
+  console.log("Admin auth is getting checked");
+  const token = "abc";
+  const isAdmin = token === "abc";
+  if (!isAdmin) {
+    res.status(401).send("User is not an admin");
+  } else {
+    next();
+  }
+});
 
-// app.use(
-//   "/user",
-//   (req, res, next) => {
-//     console.log("Route handler 1");
-//     next();
-//     res.send("Route handler 1");
-//   },
-//   (req, res) => {
-//     console.log("Route handler 2");
-//     res.send("Route handler 2");
-//   }
-// );
+app.get("/admin/getAllData", (req, res) => {
+  res.send("All data is sent");
+});
+
+app.delete("/admin/deleteData", (req, res) => {
+  res.send("Deleted the data");
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on PORT ${PORT}`);
