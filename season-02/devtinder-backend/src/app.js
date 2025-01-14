bc2fd40267ada6fa70a13965e45752990aa1230b
sharadindu das <sharadindudas774@gmail.com>
2025-01-14 11:51:32 +0530

    feat: Added dynamic signup feature

--- season-02/devtinder-backend/src/app.js
+++ season-02/devtinder-backend/src/app.js
@@ -5,25 +5,52 @@ const UserModel = require("./models/user");
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(express.json());
+
 // Signup
 app.post("/signup", async (req, res) => {
   try {
+    // Get data from request body
+    const userdata = req.body;
+
     // Create a new instance of the user
-    const newuser = new UserModel({
-      firstName: "Sharadindu",
-      lastName: "Das",
-      email: "abc@gmail.com",
-      password: "password",
-    });
+    const newuser = new UserModel(userdata);
 
     // Saving the user
     await newuser.save();
 
     // Return the response
-    res.status(200).send("User added successfully");
+    res.status(201).send("User added successfully");
   } catch (err) {
-    res.status(500).send("Error while signing up the user");
+    res.status(500).send("Internal Server Error occurred");
   }
+});
+
+// Get user details
+app.get("/user", async (req, res) => {
+  try {
+    // Get email from request body
+    const { email } = req.body;
+
+    // Check if the user exists in the db or not
+    const userExists = await UserModel.findOne({ email });
+    if (!userExists) {
+      res.status(404).send("User does not exists");
+    } else {
+      res.status(200).send(userExists);
+    }
+  } catch (err) {
+    res.status(500).send("Internal Server Error occurred");
+  }
+});
+
+// Feed api - get all users
+app.get("/feed", async (req, res) => {
+  // Find all users
+  const users = await UserModel.find({});
+
+  // Return the response
+  res.status(200).send(users);
 });
 
 // Connection to database
